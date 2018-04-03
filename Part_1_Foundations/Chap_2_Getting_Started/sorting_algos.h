a77d57234fe2fa08bf56314a04d9fbac3212827a
christian <cd@palmtreestatistics.com>
2018-04-03 14:37:17 +0200

    Added BinaryInsertionSort to sorting_algos

--- Part_1_Foundations/Chap_2_Getting_Started/sorting_algos.h
+++ Part_1_Foundations/Chap_2_Getting_Started/sorting_algos.h
@@ -1,2 +1,7 @@
 void InsertionSort(int array[], int len);
+void BinaryInsertionSort(int array[], int len);
+int binarySearchUpperBound(int array[], int start, int end, int value);
 void ReversedInsertionSort(int array[], int len);
+void MergeSort(int array[], int len);
+void mergeSort(int array[], int start, int end);
+void merge(int array[], int start, int mid, int end);
--- Part_1_Foundations/Chap_2_Getting_Started/time_sorting_algos.c
+++ Part_1_Foundations/Chap_2_Getting_Started/time_sorting_algos.c
@@ -4,32 +4,51 @@
 #include "utils.h"
 #include "sorting_algos.h"
 
+void timeSortingAlgoWorstCase(void (*sortingAlgo)(int*, int), char *name, FILE *csvfile, int sizes[], int sizes_len, int reps);
 
 int main(int argc, char** argv) {
-	float time_spent;
-	int i, j, size, k;
 	const int reps = 100;
 	int sizes[argc-1];
+	int i;
 
 	for (i=0; i<argc-1; i++) {
 		sizes[i] = atoi(argv[i+1]);
 	} 
 
 	int sizes_len = sizeof(sizes) / sizeof(sizes[0]);
-	size = sizes[0];
-	int *dynarray = malloc(size * sizeof *dynarray);
-	float avg_time;
-	int avg_count;
 
-	printf("\nTiming insertion sort\n");
+	printf("Timing sorting algos\n");
 	printf("Array sizes chosen: ");
 	print_array(sizes, sizes_len);
-	FILE *csvfile = fopen("insertion_sort.dat", "w");
+
+	FILE *csvfile; 
+	csvfile = fopen("time_measurments.csv", "w");
+	fprintf(csvfile, "size,avg_time,name\n");
+	fclose(csvfile);
+	csvfile = fopen("time_measurments.csv", "a");
+
 	if (csvfile == NULL) {
-		printf("Error opening .dat file!");
+		printf("Error opening csv file!");
 		exit(1);
 	}
+	timeSortingAlgoWorstCase(*InsertionSort, "insertion_sort", csvfile, sizes, sizes_len, reps);
+	timeSortingAlgoWorstCase(*MergeSort, "merge_sort", csvfile, sizes, sizes_len, reps);
+	fclose(csvfile);
+
 	
+	return 0;
+}
+
+
+
+void timeSortingAlgoWorstCase(void (*sortingAlgo)(int*, int), char *name, FILE *csvfile, int sizes[], int sizes_len , int reps) {
+	float time_spent, avg_time;
+	clock_t begin, end;
+	int i, j, size, k, avg_count;
+	
+	size = sizes[0];
+	int *dynarray = malloc(size * sizeof *dynarray);
+	printf("\nTimming %s\n", name);
 	for(k=0; k < sizes_len; k++) {
 		size = sizes[k];
 		dynarray = realloc(dynarray, size * sizeof *dynarray);	
@@ -39,20 +58,13 @@ int main(int argc, char** argv) {
 			for (j=0; j<size; j++) {
 				dynarray[j] = size-j;
 			}
-			clock_t begin = clock();
-			InsertionSort(dynarray, size);
-			clock_t end = clock();
+			begin = clock();
+			(*sortingAlgo)(dynarray, size);
+			end = clock();
 			time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
 			mean(time_spent, &avg_time, &avg_count);
 		}
 		printf("Average time spent: %.3e over %d reps for array of size %d\n", avg_time, reps, size);
-		fprintf(csvfile, "%d %e\n", size, avg_time);
+		fprintf(csvfile, "%d, %e, %s\n", size, avg_time, name);
 	}
-	fclose(csvfile);
-	
-	return 0;
 }
-
-
-
-
