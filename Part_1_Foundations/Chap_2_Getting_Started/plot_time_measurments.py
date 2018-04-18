c3818975de27410883a4f67560a6b55e05f0de85
christian <cd@palmtreestatistics.com>
2018-04-18 10:21:06 +0200

    Changed y-axis on sorting algos running timeplot and ran new simulation with array size of 100

--- Part_1_Foundations/Chap_2_Getting_Started/plot_time_measurments.py
+++ Part_1_Foundations/Chap_2_Getting_Started/plot_time_measurments.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv("time_measurments.csv")
 
+# Rescaling seconds to miliseconds
+df['avg_time'] = df['avg_time']*1000
+
 print(df)
 
 
@@ -11,8 +14,9 @@ for name, group in df.groupby(by='name'):
 	group.plot(x='size', y='avg_time', ax=ax, label=name)
 
 
-plt.title("Worst case timing")
-plt.xlabel("Array size")
-plt.ylabel("Average time (sec)")
+ax.set_title("Worst case timing")
+ax.set_xlabel("Array size")
+ax.set_ylabel("Average time (milisec)")
+ax.ticklabel_format(style='sci', axis='y', scilimits=(-3, 3))
 # plt.show()
 plt.savefig("time_measurments.png")
