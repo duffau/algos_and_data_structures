16a5a09b08dedb2cc1ba6091d1b84b2ee20e0802
christian <cd@palmtreestatistics.com>
2018-04-20 22:28:15 +0200

    Added plot af Polynomial eval time measurments and renamed time measurmants plot of Sorting algos

--- Part_1_Foundations/Chap_2_Getting_Started/plot_time_measurments.py
+++ Part_1_Foundations/Chap_2_Getting_Started/plot_time_measurments.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("time_measurments.csv")
+time_measurments = [("sorting_algos_time_measurments.csv", "Worst case timing of sorting algos"),("poly_algos_time_measurments.csv", "Timing of polynomial evaluation algos")]
+for filename, title in time_measurments:
+	df = pd.read_csv(filename)
 
-# Rescaling seconds to miliseconds
-df['avg_time'] = df['avg_time']*1000
+	# Rescaling seconds to miliseconds
+	df['avg_time'] = df['avg_time']*1000
 
-print(df)
-
-
-fig, ax = plt.subplots()
-
-for name, group in df.groupby(by='name'):
-	group.plot(x='size', y='avg_time', ax=ax, label=name)
+	print(df)
 
 
-ax.set_title("Worst case timing")
-ax.set_xlabel("Array size")
-ax.set_ylabel("Average time (milisec)")
-ax.ticklabel_format(style='sci', axis='y', scilimits=(-3, 3))
-# plt.show()
-plt.savefig("time_measurments.png")
+	fig, ax = plt.subplots()
+
+	for name, group in df.groupby(by='name'):
+		group.plot(x='size', y='avg_time', ax=ax, label=name)
+
+
+	ax.set_title(title)
+	ax.set_xlabel("Array size")
+	ax.set_ylabel("Average time (milisec)")
+	ax.ticklabel_format(style='sci', axis='y', scilimits=(-3, 3))
+	# plt.show()
+	plt.savefig("{}.png".format(filename.replace('.csv', '')))
---
+++ Part_1_Foundations/Chap_2_Getting_Started/time_polynomial_eval.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "utils.h"
+#include "polynomial.h"
+
+void timePolyEvalWorstCase(float (*polyEvalAlgo)(float*, int, float), char *name, FILE *csvfile, int sizes[], int sizes_len, int reps);
+
+int main(int argc, char** argv) {
+	int reps = 10000;
+	int start_size = 1;
+	int end_size = 50;
+	int array_incr = 1;
+	int optind;
+
+	for (optind = 1; optind < argc && argv[optind][0] == '-'; optind+=2) {
+        switch (argv[optind][1]) {
+        case 's': start_size = atoi(argv[optind+1]); break;
+        case 'e': end_size = atoi(argv[optind+1]); break;
+        case 'r': reps = atoi(argv[optind+1]); break;
+        default:
+            fprintf(stderr, "Usage: %s [-ilw] [file...]\n", argv[0]);
+            exit(EXIT_FAILURE);
+    	}
+	}
+    int sizes_len = (end_size - start_size)/array_incr + 1;
+	int sizes[sizes_len];
+	//int sizes_len = sizeof(sizes) / sizeof(sizes[0]);
+	int i;
+	for (i=0; i< sizes_len; i++) {
+		if (i == 0) {
+			sizes[i] = start_size;
+		} else {
+			sizes[i] = sizes[i-1] + array_incr;
+		}
+	}
+
+
+	printf("Timing Polynimal evaluation algos\n");
+	printf("Reps: %d\n", reps);
+	printf("Array sizes chosen: ");
+	print_array(sizes, sizes_len);
+
+	FILE *csvfile; 
+	csvfile = fopen("poly_algos_time_measurments.csv", "w");
+	fprintf(csvfile, "size,avg_time,name\n");
+	fclose(csvfile);
+	csvfile = fopen("poly_algos_time_measurments.csv", "a");
+
+	if (csvfile == NULL) {
+		printf("Error opening csv file!");
+		exit(1);
+	}
+	timePolyEvalWorstCase(*PolynomialHorner, "Horners Rule", csvfile, sizes, sizes_len, reps);
+	timePolyEvalWorstCase(*PolynomialNaive, "Naive", csvfile, sizes, sizes_len, reps);
+	timePolyEvalWorstCase(*PolynomialIterative, "Iterative", csvfile, sizes, sizes_len, reps);
+	fclose(csvfile);
+
+	
+	return 0;
+}
+
+
+
+void timePolyEvalWorstCase(float (*polyEvalAlgo)(float*, int, float), char *name, FILE *csvfile, int sizes[], int sizes_len , int reps) {
+	float time_spent, avg_time;
+	clock_t begin, end;
+	int i, j, size, k, avg_count;
+	
+	size = sizes[0];
+	float *dynarray = malloc(size * sizeof *dynarray);
+	printf("\nTiming %s\n", name);
+	for(k=0; k < sizes_len; k++) {
+		size = sizes[k];
+		dynarray = realloc(dynarray, size * sizeof *dynarray);	
+		avg_time = 0.0;
+		avg_count = 0;
+		for(i=0; i < reps; i++) {
+			for (j=0; j<size; j++) {
+				dynarray[j] = 1.0;
+			}
+			begin = clock();
+			(*polyEvalAlgo)(dynarray, size, 2.0);
+			end = clock();
+			time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+			mean(time_spent, &avg_time, &avg_count);
+		}
+		printf("Average time spent: %.3e over %d reps for degree %d\n", avg_time, reps, size);
+		fprintf(csvfile, "%d, %e, %s\n", size, avg_time, name);
+	}
+}
