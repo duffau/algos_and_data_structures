26d7f501e9ff43151a4fb8d2602b8125e2526d52
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2018-09-15 23:37:56 +0200

    Updated inversion exercise answer

---
+++ Part_1_Foundations/Chap_2_Getting_Started/invertions.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main(int argc, char const *argv[])
+{
+    size = 100;
+    int *dynarray = malloc(size * sizeof *dynarray);
+    
+
+}
