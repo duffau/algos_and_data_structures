e262d1dc3ba415efb1247423339b584fe9a67d8a
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2019-07-16 16:26:35 +0200

    Removed binary file and updated gitignore

--- venv_creation.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-virtualenv --no-site-packages  -p /usr/bin/python2.7 venv
-venv/bin/pip install --upgrade pip
-venv/bin/pip install -r requirements.txt --process-dependency-links
