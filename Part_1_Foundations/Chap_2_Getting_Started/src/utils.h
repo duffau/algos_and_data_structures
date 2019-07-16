79d2563d5eacd0bb31a6c5ed4529b48b09bd9a97
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2019-07-16 10:11:02 +0200

    Rearanged files in source, png and exercise folders

--- Part_1_Foundations/Chap_2_Getting_Started/src/utils.h
+++ Part_1_Foundations/Chap_2_Getting_Started/src/utils.h
@@ -1,4 +1,5 @@
 void mean(float x, float *current_mean, int *count);
+void mean(float x, float *current_mean, int *count);
 void print_array(int array[], int len);
 void checkSortedArray(int array[], int len);
 #define arrayLength(a) (sizeof(a)/sizeof(a[0]))
